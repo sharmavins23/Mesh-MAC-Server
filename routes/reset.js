d208b6c7cffd071e3f567b162a3ee8b1abe69941
sharmavins23 <sharmavins23@gmail.com>
2022-07-05 14:53:55 -0400

    created device count reset command

---
+++ routes/reset.js
@@ -0,0 +1,19 @@
+// ./routes/reset.js
+// Reset all device response counts to zero to start counting again
+
+// ===== Route functionality ===================================================
+
+function reset(app) {
+    app.get("/reset", (request, response) => {
+        // Iterate through and set all device response counts to zero
+        for (const [key, value] of Object.entries(global.deviceList)) {
+            global.deviceList[key] = 0;
+        }
+
+        response
+            .status(302) // HTTP status code 302: Redirect
+            .redirect("/"); // Redirect back to the homepage for resetting
+    });
+}
+
+module.exports = reset;
